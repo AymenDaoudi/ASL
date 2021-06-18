73da96018ffaf8d81f3adcbae08401dc402c8aab
Aymen Daoudi <aymen.daoudi@oneocean.com>
2021-06-18 02:48:46 -0400

    Refactoring

---
+++ CodeGenerator/CodeFileAbstractGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace CodeGenerator
+{
+    public abstract class CodeFileAbstractGenerator
+    {
+        protected readonly string @namespace;
+        protected readonly string @class;
+        protected readonly TypeAttributes modifier;
+        protected readonly bool isStatic;
+        protected readonly string[] usings;
+
+        protected Dictionary<string, string> codeSubsitutions;
+        protected ICollection<CodeMemberMethod> methods = new List<CodeMemberMethod>();
+
+        public CodeFileAbstractGenerator(
+            string @namespace,
+            string @class,
+            TypeAttributes modifier,
+            bool isStatic,
+            params string[] usings
+        )
+        {
+            this.@namespace = @namespace;
+            this.@class = @class;
+            this.modifier = modifier;
+            this.isStatic = isStatic;
+            this.usings = usings;
+            this.codeSubsitutions = new Dictionary<string, string>();
+        }
+
+        public virtual void CreateFile(string filePath) => CreateFileDefault(filePath);
+
+        protected void CreateFileDefault(string filePath)
+        {
+            var codeCompileUnit = GenerateCode();
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+
+            using (FileStream fs = File.Create(filePath))
+            {
+                using (StreamWriter sourceWriter = new StreamWriter(fs))
+                {
+                    using (StringWriter stringWriter = new StringWriter())
+                    {
+                        provider.GenerateCodeFromCompileUnit(
+                            codeCompileUnit,
+                            stringWriter,
+                            options
+                        );
+
+                        var code = stringWriter.ToString();
+                        foreach (var codeSubstituion in codeSubsitutions)
+                        {
+                            code = code.Replace(codeSubstituion.Key, codeSubstituion.Value);
+                        }
+
+                        sourceWriter.Write(code);
+                    }
+                }
+            }
+        }
+
+        protected abstract CodeCompileUnit GenerateCode();
+
+    }
+}
---
+++ CodeGenerator/IServiceCollectionExtensionsCodeGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom;
+using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeGenerator
+{
+    public class IServiceCollectionExtensionsCodeGenerator : ClassCodeGenerator
+    {
+        public IServiceCollectionExtensionsCodeGenerator(
+            string @namespace,
+            string @class,
+            TypeAttributes modifier,
+            bool isStatic,
+            params string[] usings
+        ) : base(
+            @namespace,
+            @class,
+            modifier,
+            isStatic,
+            usings
+        )
+        {
+            var servicesRegistrationMethod = CreateServicesRegistrationMethod("RegisterRepositories");
+            var repositoriesRegistrationMethod = CreateServicesRegistrationMethod("RegisterServices");
+
+            methods.Add(servicesRegistrationMethod);
+            methods.Add(repositoriesRegistrationMethod);
+        }
+
+        private CodeMemberMethod CreateServicesRegistrationMethod(string methodName)
+        {
+            CodeMemberMethod registerRepositoriesMethod = new CodeMemberMethod();
+            
+            registerRepositoriesMethod.Attributes = MemberAttributes.Public | MemberAttributes.Static;
+            registerRepositoriesMethod.Name = methodName;
+            
+            var servicesParam = new CodeParameterDeclarationExpression($"this {nameof(IServiceCollection)}", "services");
+            
+            registerRepositoriesMethod.Parameters.Add(servicesParam);
+            registerRepositoriesMethod.ReturnType =
+                new CodeTypeReference(nameof(IServiceCollection), CodeTypeReferenceOptions.GenericTypeParameter);
+
+            var returnStatement = new CodeMethodReturnStatement();
+            returnStatement.Expression = new CodeArgumentReferenceExpression("services");
+
+            registerRepositoriesMethod.Statements.Add(returnStatement);
+
+            return registerRepositoriesMethod;
+        }
+    }
+}
