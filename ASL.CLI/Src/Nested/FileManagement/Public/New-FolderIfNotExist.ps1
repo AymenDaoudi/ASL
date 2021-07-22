dc07dd8789d771bb33151b151dce62a41c0a2707
Aymen Daoudi <aymen.daoudi@oneocean.com>
2021-07-22 04:34:11 -0400

    Implemented service creation

---
+++ ASL.CLI/Src/Nested/FileManagement/Public/New-FolderIfNotExist.ps1
@@ -0,0 +1,27 @@
+﻿function New-FolderIfNotExist {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string] $Location = (Get-Location),
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string] $Name
+    )
+
+    begin
+    {
+    }
+    process
+    {
+        if (!(test-path "$Location\$Name")) {
+            $NewDirectory = New-Item -ItemType Directory -Name $Name -Path $Location
+        }
+        else {
+            $NewDirectory = "$Location\$Name"
+        }
+
+        return $NewDirectory
+    }
+    end
+    {
+    }
+}
---
+++ ASL.CLI/Src/Private/Get-SolutionRootDirectory.ps1
@@ -0,0 +1,35 @@
+﻿function Get-SolutionRootDirectory {
+    [CmdletBinding()]
+    param (
+        $currentLocation = (Get-Location)
+    )
+
+    begin
+    {
+    }
+    process
+    {
+        if (!$currentLocation) {
+            $currentLocation = Get-Location
+        }
+
+        $ContainsSolutionFile = Get-ChildItem "$currentLocation\*" -Include *.sln | Measure-Object
+
+        if ($ContainsSolutionFile.Count -ne 0) {
+            return $currentLocation
+        }
+
+        $HasParent = (get-item $currentLocation).Parent
+
+        if (!$HasParent) {
+            return $null
+        }
+
+        $Parent = ((get-item $currentLocation).Parent).FullName
+        return Get-SolutionRootDirectory $Parent
+
+    }
+    end
+    {
+    }
+}
